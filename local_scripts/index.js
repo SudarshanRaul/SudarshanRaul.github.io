27af8ea880a028818f6b75a0c049a15cb283355f
SudarshanRaul <mr.sudarshanraul@gmail.com>
2017-11-27 11:58:55 +0530

    Update index.js

--- local_scripts/index.js
+++ local_scripts/index.js
@@ -8,10 +8,8 @@ $(document).ready(function(){
 	
 	//Reset : scroll to 0
 	function reset() {
-		console.log("resetting");
 		scrollHeight = 0;
 		if(scrollTimer) {
-			console.log("clearing " + scrollTimer);
 			clearInterval(scrollTimer);
 		}
 		$('body,html').animate({scrollTop: scrollHeight}, scrollTime-200);
@@ -19,13 +17,10 @@ $(document).ready(function(){
 	}
 	reset();
 	
-	//scrollTimer = window.setInterval (scrollBody, scrollDelay);
-	
 	function scrollBody() {
 		if(scrollHeight >= documentHeight) {
 			reset();
 		} else {
-			console.log("scrooling to " + scrollHeight);
 			$('body,html').animate({scrollTop: scrollHeight}, scrollTime);
 			scrollHeight = scrollHeight + (windowHeight * 1.75);
 		}
