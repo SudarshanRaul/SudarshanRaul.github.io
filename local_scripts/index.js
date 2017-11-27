bb66490931d82121328c47f99b4d363ede5763c7
SudarshanRaul <mr.sudarshanraul@gmail.com>
2017-11-27 10:46:13 +0530

    Update index.js

--- local_scripts/index.js
+++ local_scripts/index.js
@@ -1,16 +1,27 @@
 $(document).ready(function(){
 	var windowHeight = $(window).height();
 	var documentHeight = $(document).height();
-	var scrollHeight = windowHeight * 1.5;
 	var scrollTime = 700;
 	var scrollDelay = 2000;
+	var scrollHeight;
 	
 	//Reset : scroll to 0
-	$('body,html').animate({scrollTop: 0}, scrollTime);
+	function reset() {
+		scrollHeight = windowHeight * 2;
+		$('body,html').animate({scrollTop: 0}, scrollTime);
+	}
+	reset();
 	
-	window.setInterval (function() {
+	var scrollTimer = window.setInterval (scrollBody, scrollDelay);
+	
+	function scrollBody() {
+		if(scrollHeight >= documentHeight){
+			reset();
+			clearInterval(scrollTimer);
+			scrollTimer = window.setInterval (scrollBody, scrollDelay);
+		}
 		$('body,html').animate({scrollTop: scrollHeight}, scrollTime);
 		console.log(scrollHeight);
-		scrollHeight = scrollHeight + (windowHeight / 2);
-	}, scrollDelay);
+		scrollHeight = scrollHeight + (windowHeight);
+	}
 });
