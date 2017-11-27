86a801ec6bd3474634824a0075048b9cc04bb7ca
SudarshanRaul <mr.sudarshanraul@gmail.com>
2017-11-27 11:02:20 +0530

    Update index.js

--- local_scripts/index.js
+++ local_scripts/index.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	var windowHeight = $(window).height();
 	var documentHeight = $(document).height();
-	var scrollTime = 5000;
-	var scrollDelay = 2000;
+	var scrollTime = 1000;
+	var scrollDelay = 5000;
 	var scrollHeight;
 	
 	//Reset : scroll to 0
 	function reset() {
 		scrollHeight = windowHeight * 1.75;
-		$('body,html').animate({scrollTop: 0}, scrollTime);
+		$('body,html').animate({scrollTop: 0}, scrollTime-200);
 	}
 	reset();
 	
@@ -18,7 +18,9 @@ $(document).ready(function(){
 		if(scrollHeight >= documentHeight){
 			reset();
 			clearInterval(scrollTimer);
-			scrollTimer = window.setInterval (scrollBody, scrollDelay);
+			window.setTimeout(function() {
+				scrollTimer = window.setInterval (scrollBody, scrollDelay);
+			}, scrollDelay);
 		}
 		$('body,html').animate({scrollTop: scrollHeight}, scrollTime);
 		console.log(scrollHeight);
