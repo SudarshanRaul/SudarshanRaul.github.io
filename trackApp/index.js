c6b8a8f921b293d58213dc30c8132a4576ac7354
sudarshan <sudarshan@sudarshan.com>
2017-12-09 18:28:14 +0530

    removed undeveloped code

--- trackApp/index.js
+++
@@ -1,9 +0,0 @@
-class TrackApp extends React.Component{
-    
-    render() {
-        const {expTrackStore} = this.props;
-        return (
-            <AddExpence expTrackStore={expTrackStore}/>
-        )
-    }
-};
