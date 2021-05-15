7e8ff5fd0120926ecde6b9997fb9b86d455e5b2c
Sudarshan Raul <sudarshan_raul@intuit.com>
2021-05-15 16:38:06 +0530

    loading script after body on load

--- local_scripts/account.js
+++ local_scripts/account.js
@@ -1,16 +1,3 @@
-(function () {
-    if(getProfile()) {
-        document.querySelectorAll(".account-sign-in")[0].style.display = 'none';
-        document.querySelectorAll("#gUserName")[0].innerHTML = getProfile().getName();
-        document.querySelectorAll("gUserImg")[0].src = getProfile().getImageUrl();
-        document.querySelectorAll(".account-signed-in")[0].style.display = "block";
-    } else {
-        document.querySelectorAll(".account-sign-in")[0].style.display = 'block';
-        document.querySelectorAll(".account-signed-in")[0].style.display = "none";
-    }
-
-})();
-
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     sessionStorage.setItem("profile", JSON.stringify(profile));
@@ -24,4 +11,17 @@ function getProfile() {
     } else {
         return false;
     }
+}
+
+function bodyLoad () {
+    if(getProfile()) {
+        document.querySelectorAll(".account-sign-in")[0].style.display = 'none';
+        document.querySelectorAll("#gUserName")[0].innerHTML = getProfile().getName();
+        document.querySelectorAll("gUserImg")[0].src = getProfile().getImageUrl();
+        document.querySelectorAll(".account-signed-in")[0].style.display = "block";
+    } else {
+        document.querySelectorAll(".account-sign-in")[0].style.display = 'block';
+        document.querySelectorAll(".account-signed-in")[0].style.display = "none";
+    }
+
 }
