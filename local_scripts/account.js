ceec688e9b8723881fc73ef2764b58e8312f14da
Sudarshan Raul <sudarshan_raul@intuit.com>
2021-05-15 14:49:29 +0530

    Adding user name in top right corner

---
+++ local_scripts/account.js
@@ -0,0 +1,27 @@
+(function () {
+    if(getProfile()) {
+        document.querySelectorAll(".account-sign-in")[0].style.display = 'none';
+        document.querySelectorAll("#gUserName")[0].innerHTML = getProfile().getName();
+        document.querySelectorAll("gUserImg")[0].src = getProfile().getImageUrl();
+        document.querySelectorAll(".account-signed-in")[0].style.display = "block";
+    } else {
+        document.querySelectorAll(".account-sign-in")[0].style.display = 'block';
+        document.querySelectorAll(".account-signed-in")[0].style.display = "none";
+    }
+
+})();
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    sessionStorage.setItem("profile", JSON.stringify(profile));
+}
+
+function getProfile() {
+    var profileJson = sessionStorage.getItem("profile");
+    if(profileJson) {
+        var profile = JSON.parse(profileJson);
+        return (profile);
+    } else {
+        return false;
+    }
+}
